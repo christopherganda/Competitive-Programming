52321df26da4030d07bbd875f7f5cb55fafa5400
christopher.ganda <christopher.ganda@mikroskil.ac.id>
2017-09-18 13:32:37 +0700

    first commit: add template

---
+++ template.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define freinput "input.txt","r",stdin
+#define freoutput "output.txt","w",stdout
+#define mp make_pair
+#define fi first
+#define sc second
+#define ellapse printf("Time : %0.3lf\n",clock()*1.0/CLOCKS_PER_SEC);
+typedef long long ll;
+typedef unsigned long int uld;
+typedef vector<int> vi;
+typedef vector<string> vs;
+typedef pair<int,int> pii;
+int main(){
+    #ifdef chris
+    freopen(freinput);
+    freopen(freoutput);
+    #endif // chris
+
+}
